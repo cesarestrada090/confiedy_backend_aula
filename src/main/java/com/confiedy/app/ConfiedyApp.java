0fc346d6b6a6f24d393b9bc895b72922643b763a
Estrada <cesarestrada090@gmail.com>
2023-08-28 00:33:21 -0500

    remove unnecsary stufs

--- src/main/java/com/confiedy/app/ConfiedyApp.java
+++ src/main/java/com/confiedy/app/ConfiedyApp.java
@@ -1,6 +1,4 @@
 package com.confiedy.app;
-
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableAutoConfiguration
-@OpenAPIDefinition
 public class ConfiedyApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfiedyApp.class, args);
--- src/main/java/com/confiedy/app/ConfiedyAppConfig.java
+++ src/main/java/com/confiedy/app/ConfiedyAppConfig.java
@@ -1,9 +1,5 @@
 package com.confiedy.app;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan
@@ -15,24 +11,4 @@ class ConfiedyAppConfig {
         return new ModelMapper();
     }
 
-    @Bean
-    public OpenAPI createOpenAPI(
-            @Value("${springdoc.title}") String title,
-            @Value("${springdoc.description}") String description,
-            @Value("${springdoc.version}") String version,
-            @Value("${springdoc.contact-name}") String contactName,
-            @Value("${springdoc.contact-email}") String contactEmail,
-            @Value("${springdoc.contact-url}") String contactUrl
-    ) {
-        var contact = new Contact();
-        contact.setName(contactName);
-        contact.email(contactEmail);
-        contact.url(contactUrl);
-        return new OpenAPI()
-                .info(new Info()
-                        .title(title)
-                        .description(description)
-                        .version(version)
-                        .contact(contact));
-    }
 }
