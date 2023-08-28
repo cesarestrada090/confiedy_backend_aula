fec63be1f988f5108e8a69e4c5f54c0fcec04f3c
Estrada <cesarestrada090@gmail.com>
2023-08-28 00:36:20 -0500

    remove unnecsary stufs

--- src/main/java/com/confiedy/app/repository/UsuarioRepository.java
+++ src/main/java/com/confiedy/app/repository/UsuarioRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findUsuarioByUsernameAndPassword(String usuario, String password);
 }
