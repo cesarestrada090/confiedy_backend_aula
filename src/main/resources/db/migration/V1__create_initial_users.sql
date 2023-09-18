89dbdbba7e9710fb48b240c08a6f62b933d99514
Estrada <cesarestrada090@gmail.com>
2023-09-18 00:06:02 -0500

    add initial users

--- src/main/resources/db/migration/V1__create_initial_users.sql
+++ src/main/resources/db/migration/V1__create_initial_users.sql
@@ -11,7 +11,7 @@ INSERT INTO docente(id,nombre, apellidos, link_cv) VALUES(12,'Aarón','Diaz', 'P
 insert into curso (id,codigo_curso,nombre_curso,ciclo,docente_id) values(20,'MAT124','Matematica 01','Varios',10);
 
 -- Evaluaciones para curso Matemática
-INSERT INTO evaluacion_curso (id,curso_id, numero_unidad, horario, link_material, link_clase) VALUES(5,20, 'Unidad I', 'Miércoles y Jueves 4-6pm ', 'https://pucp.zoom.us/j/98878411077', 'https://drive.google.com/drive/u/2/folders/1n8K2XrzV_t8UnwUF58C8A7Z5K9HsXTR4');
+INSERT INTO evaluacion_curso (id,curso_id, numero_unidad, horario, link_material, link_clase) VALUES(5,20, 'Unidad I', 'Miércoles y Jueves 4-6pm ', 'https://drive.google.com/drive/u/2/folders/1n8K2XrzV_t8UnwUF58C8A7Z5K9HsXTR4', 'https://pucp.zoom.us/j/98878411077');
 
 -- Alumnos
 insert into alumno (id,nombre,apellido,carrera, fecha_nacimiento) values (100,'Leonardo','Velasquez','Pendiente',CURDATE());
